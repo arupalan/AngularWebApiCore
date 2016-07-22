fd820e5753459c2c497da8c66050b0558c600418
Arup Banerjee <Arup Banerjee>
2016-07-22 07:04:49 +0100

    Added _Code specific to ApiError

---
+++ src/AngularWebApiCore/_Code/ApiError.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace AngularWebApiCore._Code
+{
+    public class ApiError
+    {
+        public bool isError { get; set; }
+        public string message { get; set; }
+        public string detail { get; set; }
+        public Dictionary<string, string> errors { get; set; }
+
+        public ApiError(string message)
+        {
+            this.message = message;
+            this.isError = true;
+        }
+
+        public ApiError(ModelStateDictionary modelState)
+        {
+            this.isError = true;
+            if (modelState != null && modelState.Any(m => m.Value.Errors.Count > 0))
+            {
+                message = "Please correct the specified errors and try again.";
+                //errors = modelState.SelectMany(m => m.Value.Errors).ToDictionary(m => m.Key, m=> m.ErrorMessage);
+                //errors = modelState.SelectMany(m => m.Value.Errors.Select( me => new KeyValuePair<string,string>( m.Key,me.ErrorMessage) ));
+                //errors = modelState.SelectMany(m => m.Value.Errors.Select(me => new ModelError { FieldName = m.Key, ErrorMessage = me.ErrorMessage }));
+            }
+        }
+    }
+}
---
+++ src/AngularWebApiCore/_Code/ApiException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularWebApiCore._Code
+{
+    public class ApiException : Exception
+    {
+        public int StatusCode { get; set; }
+
+        public ApiException(string message, int statusCode = 500) :
+            base(message)
+        {
+            StatusCode = statusCode;
+        }
+        public ApiException(Exception ex, int statusCode = 500) : base(ex.Message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
